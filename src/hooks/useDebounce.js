40bee9b212040103740fdc1def9a9c5c6fdcc30b
olehok <ok.olegkovalenko@gmail.com>
2026-02-13 17:21:01 +0200

    useDeb

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,36 @@
+// "use strict";
+
+// var _interopRequireWildcard = require("@babel/runtime/helpers/interopRequireWildcard").default;
+// Object.defineProperty(exports, "__esModule", {
+//   value: true
+// });
+// exports.default = useDebounce;
+// var React = _interopRequireWildcard(require("react"));
+// function useDebounce(value) {
+//   const [cacheValue, setCacheValue] = React.useState(value);
+//   React.useEffect(() => {
+//     const timeout = setTimeout(() => {
+//       setCacheValue(value);
+//     }, value.length ? 0 : 10);
+//     return () => {
+//       clearTimeout(timeout);
+//     };
+//   }, [value]);
+//   return cacheValue;
+// }
+
+import { useEffect, useState } from "react";
+
+export function useDebounce(value, delay = 400) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const id = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => clearTimeout(id);
+  }, [value, delay]);
+
+  return debouncedValue;
+}
