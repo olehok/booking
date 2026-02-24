b97fa41e4bd439a66c7504ffabe2509cbfabe084
olehok <ok.olegkovalenko@gmail.com>
2026-02-25 00:09:45 +0200

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,31 @@
 import SearchForm from "../components/SearchForm";
+import { Space, Typography } from "antd";
+
+const { Title, Text } = Typography;
 
 export default function Home() {
   return (
     <section>
-      <div >
-        <h2>Travel with Booking</h2>
-        <p>
+      <Space orientation="horizontal" size="middle">
+        <Title level={3}>Travel with </Title>
+        <Title
+          level={3}
+          italic
+          style={{
+            color: "var(--primary-color)",
+            WebkitTextStroke: "1px var(--primary-color)",
+          }}
+        >
+          Booking
+        </Title>
+      </Space>
+      <div>
+        <Text>
           Explore and book the best hotels around the world with ease and
           confidence.
-        </p>
-        <SearchForm />
+        </Text>
       </div>
+      <SearchForm />
     </section>
   );
 }
